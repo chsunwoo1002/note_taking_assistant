a155354aa3880573f9baea7194f8d6a7e579821e
Sunwoo <sunwoo.choi.se@gmail.com>
2024-08-16 16:54:12 -0400

    refactor the response object

--- services/note_creation/src/common/models/serviceResponse.ts
+++ services/note_creation/src/common/models/serviceResponse.ts
@@ -4,35 +4,35 @@ import { z } from "zod";
 export class ServiceResponse<T = null> {
   readonly success: boolean;
   readonly message: string;
-  readonly responseObject: T;
+  readonly data: T;
   readonly statusCode: number;
 
   private constructor(
     success: boolean,
     message: string,
-    responseObject: T,
+    data: T,
     statusCode: number,
   ) {
     this.success = success;
     this.message = message;
-    this.responseObject = responseObject;
+    this.data = data;
     this.statusCode = statusCode;
   }
 
   static success<T>(
     message: string,
-    responseObject: T,
+    data: T,
     statusCode: number = StatusCodes.OK,
   ) {
-    return new ServiceResponse(true, message, responseObject, statusCode);
+    return new ServiceResponse(true, message, data, statusCode);
   }
 
   static failure<T>(
     message: string,
-    responseObject: T,
+    data: T,
     statusCode: number = StatusCodes.BAD_REQUEST,
   ) {
-    return new ServiceResponse(false, message, responseObject, statusCode);
+    return new ServiceResponse(false, message, data, statusCode);
   }
 }
 
@@ -40,6 +40,6 @@ export const ServiceResponseSchema = <T extends z.ZodTypeAny>(dataSchema: T) =>
   z.object({
     success: z.boolean(),
     message: z.string(),
-    responseObject: dataSchema.optional(),
+    data: dataSchema.optional(),
     statusCode: z.number(),
   });
