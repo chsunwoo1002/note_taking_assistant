733c0597a2aa3db7eeb55f5e29c297bf6c80d40f
Sunwoo <sunwoo.choi.se@gmail.com>
2024-09-17 21:03:54 -0400

    add icon & labeled Icon button

---
+++ note-web-app/src/components/common/icon-button.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Button, ButtonProps } from "@/components/ui/button";
+import { LucideIcon } from "lucide-react";
+import withTooltip from "./withTooltip";
+
+interface IconButtonProps extends Omit<ButtonProps, "children" | "size"> {
+  icon: LucideIcon;
+  ariaLabel: string;
+}
+
+function BaseIconButton({ icon: Icon, ariaLabel, ...props }: IconButtonProps) {
+  return (
+    <Button {...props} aria-label={ariaLabel} size="icon">
+      <Icon className="h-4 w-4" />
+    </Button>
+  );
+}
+
+export const IconButton = withTooltip(BaseIconButton);
