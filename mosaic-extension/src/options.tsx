74426fba452d58715beb4dcb631966604a2b48ca
Sunwoo <sunwoo.choi.se@gmail.com>
2024-09-25 15:37:38 -0400

    design extension design

--- mosaic-extension/src/options.tsx
+++ mosaic-extension/src/options.tsx
@@ -5,10 +5,39 @@ import { useEffect, useState } from "react"
 
 import { sendToBackground } from "@plasmohq/messaging"
 
+import "@/style.css"
+
+import { Separator } from "@/components/ui/separator"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "./components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "./components/ui/form"
+import { Input } from "./components/ui/input"
+
+const formSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8)
+})
+
 function IndexOptions() {
   const { user, setUser } = useUser()
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
+  const [action, setAction] = useState<"LOGIN" | "SIGNUP">("LOGIN")
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: ""
+    }
+  })
 
   useEffect(() => {
     async function init() {
@@ -73,77 +102,107 @@ function IndexOptions() {
     })
   }
 
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
+    console.log(data)
+  }
+
   return (
-    <main
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "100%",
-        top: 240,
-        position: "relative"
-      }}>
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          width: 240,
-          justifyContent: "space-between",
-          gap: 4.2
-        }}>
-        {user && (
-          <>
-            <h3>
-              {user.email} - {user.id}
-            </h3>
-            <button
-              onClick={() => {
-                supabase.auth.signOut()
-                setUser(null)
-              }}>
-              Logout
-            </button>
-          </>
-        )}
-        {!user && (
-          <>
-            <label>Email</label>
-            <input
-              type="text"
-              placeholder="Your Username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <label>Password</label>
-            <input
-              type="password"
-              placeholder="Your password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-
-            <button
-              onClick={(e) => {
-                handleEmailLogin("SIGNUP", username, password)
-              }}>
-              Sign up
-            </button>
-            <button
-              onClick={(e) => {
-                handleEmailLogin("LOGIN", username, password)
-              }}>
-              Login
-            </button>
-
-            <button
-              onClick={(e) => {
-                handleOAuthLogin("google")
-              }}>
-              Sign in with Google
-            </button>
-          </>
-        )}
-      </div>
+    <main className="flex flex-col bg-primary text-primary-foreground w-screen h-screen justify-center items-center">
+      {user && (
+        <>
+          <Button
+            variant="secondary"
+            onClick={() => {
+              supabase.auth.signOut()
+              setUser(null)
+            }}>
+            Logout
+          </Button>
+        </>
+      )}
+      {!user && (
+        <div className="flex flex-col space-y-4">
+          <h1 className="text-2xl font-medium text-center">
+            {action === "LOGIN" ? "Sign in" : "Sign up"}
+          </h1>
+          {action === "LOGIN" ? (
+            <div className="flex flex-row gap-2 items-center">
+              <p className="text-sm ">Don't have an account? </p>
+              <Button
+                className="font-medium underline"
+                onClick={() => setAction("SIGNUP")}>
+                Sign up
+              </Button>
+            </div>
+          ) : (
+            <div className="flex flex-row gap-2 items-center">
+              <p className="text-sm">Already have an account? </p>
+              <Button
+                className="font-medium underline"
+                onClick={() => setAction("LOGIN")}>
+                Sign in
+              </Button>
+            </div>
+          )}
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <div className="flex flex-col gap-2 justify-between mb-2">
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-col gap-2">
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          className="bg-primary"
+                          placeholder="Your Email"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem className="flex flex-col gap-2">
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          className="bg-primary"
+                          placeholder="Your password"
+                          type="password"
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="mt-4">
+                <Button
+                  className="flex flex-row gap-2 w-full"
+                  variant="secondary"
+                  type="submit">
+                  {action === "LOGIN" ? "Sign in" : "Sign up"}
+                </Button>
+              </div>
+            </form>
+          </Form>
+          <Separator className="my-4" />
+          <Button
+            variant="secondary"
+            onClick={(e) => {
+              handleOAuthLogin("google")
+            }}>
+            {action === "LOGIN" ? "Sign in with Google" : "Sign up with Google"}
+          </Button>
+        </div>
+      )}
     </main>
   )
 }
--- mosaic-extension/src/popup/index.tsx
+++ mosaic-extension/src/popup/index.tsx
@@ -1,31 +1,61 @@
 import { createNote, getNotes } from "@/api/note"
 import { Button } from "@/components/ui/button"
-import { useFeatureToggle } from "@/hooks/useFeatureToggle"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Separator } from "@/components/ui/separator"
 import { useUser } from "@/hooks/useUser"
+import { Header } from "@/popup/components/header"
+import { zodResolver } from "@hookform/resolvers/zod"
 import { useEffect, useState } from "react"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
 
 import "@/style.css"
 
-import { Label } from "../components/ui/label"
-import { Switch } from "../components/ui/switch"
+import { Card } from "@/components/ui/card"
+import { ScrollArea } from "@/components/ui/scroll-area"
+import { Textarea } from "@/components/ui/textarea"
+import { TooltipProvider } from "@/components/ui/tooltip"
+import { useSelectedNote } from "@/hooks/useSelectedNote"
+
+const formSchema = z.object({
+  title: z.string().min(1),
+  instruction: z.string().optional()
+})
 
 export default function IndexPopup() {
   const { user } = useUser()
-  const { active, toggle } = useFeatureToggle()
   const [notes, setNotes] = useState<any[]>([])
-  const [title, setTitle] = useState("")
-  const [instruction, setInstruction] = useState("")
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    const { data, error } = await createNote(title, instruction)
-    if (error) {
-      console.error(error)
-    } else {
-      setNotes([...notes, data[0]])
-      setTitle("")
-      setInstruction("")
+  const { selectedNote, setSelectedNote } = useSelectedNote()
+  const [mode, setMode] = useState<"edit" | "create">("edit")
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      instruction: ""
     }
+  })
+
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
+    const { title, instruction } = data
+    createNote(title, instruction).then(({ data, error }) => {
+      if (error) {
+        console.error(error)
+        // TODO: show error to user
+      } else {
+        setSelectedNote(data.id)
+        setMode("edit")
+        setNotes([...notes, data])
+      }
+    })
   }
 
   useEffect(() => {
@@ -42,48 +72,99 @@ export default function IndexPopup() {
 
   if (user === null)
     return (
-      <div>
-        <button
+      <div className="bg-primary text-primary-foreground w-96 h-96 flex items-center justify-center">
+        <Button
+          variant="secondary"
           onClick={() => {
             window.open("options.html", "_blank")
           }}>
-          Login
-        </button>
+          Log in / Sign up
+        </Button>
       </div>
     )
 
   return (
-    <div>
-      Hello World! {user?.email}
-      <div className="flex items-center space-x-2">
-        <Switch id="active" checked={active} onCheckedChange={toggle} />
-        <Label htmlFor="active">Active</Label>
-      </div>
-      <div>
-        list
-        {notes.map((note) => (
-          <div key={note.id}>{note.title}</div>
-        ))}
-      </div>
-      <div>
-        form
-        <form onSubmit={handleSubmit}>
-          <input
-            type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            placeholder="Title"
-            required
-          />
-          <input
-            type="text"
-            value={instruction}
-            onChange={(e) => setInstruction(e.target.value)}
-            placeholder="Instruction"
-          />
-          <button type="submit">Submit</button>
-        </form>
-      </div>
+    <div className="bg-primary text-primary-foreground w-96 h-96">
+      <TooltipProvider>
+        <Header
+          mode={mode}
+          toggleMode={() => setMode(mode === "create" ? "edit" : "create")}
+        />
+        <Separator />
+        {mode === "create" ? (
+          <div className="flex flex-col gap-2 p-2">
+            <Form {...form}>
+              <form
+                className="flex flex-col gap-2"
+                onSubmit={form.handleSubmit(onSubmit)}>
+                <div className="flex flex-col gap-2 justify-between">
+                  <FormField
+                    control={form.control}
+                    name="title"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Title</FormLabel>
+                        <FormControl>
+                          <Input
+                            {...field}
+                            className="bg-primary"
+                            placeholder="Add a title"
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={form.control}
+                    name="instruction"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Instruction</FormLabel>
+                        <FormControl className="flex-grow">
+                          <Textarea
+                            {...field}
+                            className="bg-primary"
+                            placeholder="Add a instruction (optional)"
+                            rows={7}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                <div className="flex flex-col">
+                  <Button
+                    className="flex flex-row gap-2"
+                    variant="secondary"
+                    type="submit">
+                    Create
+                  </Button>
+                </div>
+              </form>
+            </Form>
+          </div>
+        ) : (
+          <div className="h-72 gap-2 p-2">
+            <h1 className="text-xl mb-2">Notes</h1>
+            <ScrollArea className="h-full w-full rounded-md border">
+              <div className="flex-col gap-2">
+                {notes.map((note) => (
+                  <Button
+                    variant={selectedNote === note.id ? "secondary" : "ghost"}
+                    className="flex w-11/12 justify-start truncate"
+                    key={note.id}
+                    onClick={() => setSelectedNote(note.id)}>
+                    {note.title}
+                    {/* <p className="text-ellipsis"></p> */}
+                  </Button>
+                ))}
+              </div>
+            </ScrollArea>
+          </div>
+        )}
+      </TooltipProvider>
     </div>
   )
 }
