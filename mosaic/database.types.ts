2231b6773dfa8ebf1cd342fd5f7324ab748846d4
Sunwoo <sunwoo.choi.se@gmail.com>
2024-09-28 01:13:48 -0400

    Update database.types.ts

--- mosaic/database.types.ts
+++ mosaic/database.types.ts
@@ -4,408 +4,448 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[];
+  | Json[]
 
 export type Database = {
   public: {
     Tables: {
+      content_references: {
+        Row: {
+          content_id: string
+          result_id: string
+          user_id: string | null
+        }
+        Insert: {
+          content_id: string
+          result_id: string
+          user_id?: string | null
+        }
+        Update: {
+          content_id?: string
+          result_id?: string
+          user_id?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "content_references_content_id_fkey"
+            columns: ["content_id"]
+            isOneToOne: false
+            referencedRelation: "note_contents"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "content_references_result_id_fkey"
+            columns: ["result_id"]
+            isOneToOne: false
+            referencedRelation: "note_results"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "content_references_user_id_fkey"
+            columns: ["user_id"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
       content_types: {
         Row: {
-          created_at: string;
-          id: number;
-          type: string;
-        };
+          created_at: string
+          id: number
+          type: string
+        }
         Insert: {
-          created_at?: string;
-          id?: number;
-          type?: string;
-        };
+          created_at?: string
+          id?: number
+          type?: string
+        }
         Update: {
-          created_at?: string;
-          id?: number;
-          type?: string;
-        };
-        Relationships: [];
-      };
+          created_at?: string
+          id?: number
+          type?: string
+        }
+        Relationships: []
+      }
       note_contents: {
         Row: {
-          content: string | null;
-          created_at: string;
-          file_url: string | null;
-          id: string;
-          note_id: string | null;
-          source_url: string | null;
-          type_id: number;
-          updated_at: string | null;
-          user_id: string;
-          vector: string | null;
-        };
+          content: string | null
+          created_at: string
+          file_url: string | null
+          id: string
+          note_id: string | null
+          source_url: string | null
+          type_id: number
+          updated_at: string | null
+          user_id: string
+          vector: string | null
+        }
         Insert: {
-          content?: string | null;
-          created_at?: string;
-          file_url?: string | null;
-          id?: string;
-          note_id?: string | null;
-          source_url?: string | null;
-          type_id: number;
-          updated_at?: string | null;
-          user_id?: string;
-          vector?: string | null;
-        };
+          content?: string | null
+          created_at?: string
+          file_url?: string | null
+          id?: string
+          note_id?: string | null
+          source_url?: string | null
+          type_id: number
+          updated_at?: string | null
+          user_id?: string
+          vector?: string | null
+        }
         Update: {
-          content?: string | null;
-          created_at?: string;
-          file_url?: string | null;
-          id?: string;
-          note_id?: string | null;
-          source_url?: string | null;
-          type_id?: number;
-          updated_at?: string | null;
-          user_id?: string;
-          vector?: string | null;
-        };
+          content?: string | null
+          created_at?: string
+          file_url?: string | null
+          id?: string
+          note_id?: string | null
+          source_url?: string | null
+          type_id?: number
+          updated_at?: string | null
+          user_id?: string
+          vector?: string | null
+        }
         Relationships: [
           {
-            foreignKeyName: "note_contents_note_id_fkey";
-            columns: ["note_id"];
-            isOneToOne: false;
-            referencedRelation: "notes";
-            referencedColumns: ["id"];
+            foreignKeyName: "note_contents_note_id_fkey"
+            columns: ["note_id"]
+            isOneToOne: false
+            referencedRelation: "notes"
+            referencedColumns: ["id"]
           },
           {
-            foreignKeyName: "note_contents_type_id_fkey";
-            columns: ["type_id"];
-            isOneToOne: false;
-            referencedRelation: "content_types";
-            referencedColumns: ["id"];
+            foreignKeyName: "note_contents_type_id_fkey"
+            columns: ["type_id"]
+            isOneToOne: false
+            referencedRelation: "content_types"
+            referencedColumns: ["id"]
           },
           {
-            foreignKeyName: "note_contents_user_id_fkey";
-            columns: ["user_id"];
-            isOneToOne: false;
-            referencedRelation: "users";
-            referencedColumns: ["id"];
+            foreignKeyName: "note_contents_user_id_fkey"
+            columns: ["user_id"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
           },
-        ];
-      };
+        ]
+      }
       note_results: {
         Row: {
-          content: string;
-          created_at: string;
-          id: string;
-          note_id: string;
-          order: number;
-          type_id: number;
-          updated_at: string;
-          user_id: string | null;
-          vector: string | null;
-        };
+          content: string
+          created_at: string
+          id: string
+          note_id: string
+          order: number
+          type_id: number
+          updated_at: string
+          user_id: string | null
+          vector: string | null
+        }
         Insert: {
-          content: string;
-          created_at?: string;
-          id?: string;
-          note_id: string;
-          order: number;
-          type_id: number;
-          updated_at?: string;
-          user_id?: string | null;
-          vector?: string | null;
-        };
+          content: string
+          created_at?: string
+          id?: string
+          note_id: string
+          order: number
+          type_id: number
+          updated_at?: string
+          user_id?: string | null
+          vector?: string | null
+        }
         Update: {
-          content?: string;
-          created_at?: string;
-          id?: string;
-          note_id?: string;
-          order?: number;
-          type_id?: number;
-          updated_at?: string;
-          user_id?: string | null;
-          vector?: string | null;
-        };
+          content?: string
+          created_at?: string
+          id?: string
+          note_id?: string
+          order?: number
+          type_id?: number
+          updated_at?: string
+          user_id?: string | null
+          vector?: string | null
+        }
         Relationships: [
           {
-            foreignKeyName: "note_results_note_id_fkey";
-            columns: ["note_id"];
-            isOneToOne: false;
-            referencedRelation: "notes";
-            referencedColumns: ["id"];
+            foreignKeyName: "note_results_note_id_fkey"
+            columns: ["note_id"]
+            isOneToOne: false
+            referencedRelation: "notes"
+            referencedColumns: ["id"]
           },
           {
-            foreignKeyName: "note_results_type_id_fkey";
-            columns: ["type_id"];
-            isOneToOne: false;
-            referencedRelation: "note_type";
-            referencedColumns: ["id"];
+            foreignKeyName: "note_results_type_id_fkey"
+            columns: ["type_id"]
+            isOneToOne: false
+            referencedRelation: "note_type"
+            referencedColumns: ["id"]
           },
           {
-            foreignKeyName: "note_results_user_id_fkey";
-            columns: ["user_id"];
-            isOneToOne: false;
-            referencedRelation: "users";
-            referencedColumns: ["id"];
+            foreignKeyName: "note_results_user_id_fkey"
+            columns: ["user_id"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
           },
-        ];
-      };
+        ]
+      }
       note_type: {
         Row: {
-          created_at: string;
-          id: number;
-          type: string;
-        };
+          created_at: string
+          id: number
+          type: string
+        }
         Insert: {
-          created_at?: string;
-          id?: number;
-          type: string;
-        };
+          created_at?: string
+          id?: number
+          type: string
+        }
         Update: {
-          created_at?: string;
-          id?: number;
-          type?: string;
-        };
-        Relationships: [];
-      };
+          created_at?: string
+          id?: number
+          type?: string
+        }
+        Relationships: []
+      }
       notes: {
         Row: {
-          created_at: string;
-          id: string;
-          instruction: string | null;
-          title: string;
-          updated_at: string;
-          user_id: string;
-          vector: string | null;
-        };
+          created_at: string
+          id: string
+          instruction: string | null
+          title: string
+          updated_at: string
+          user_id: string
+          vector: string | null
+        }
         Insert: {
-          created_at?: string;
-          id?: string;
-          instruction?: string | null;
-          title: string;
-          updated_at?: string;
-          user_id?: string;
-          vector?: string | null;
-        };
+          created_at?: string
+          id?: string
+          instruction?: string | null
+          title: string
+          updated_at?: string
+          user_id?: string
+          vector?: string | null
+        }
         Update: {
-          created_at?: string;
-          id?: string;
-          instruction?: string | null;
-          title?: string;
-          updated_at?: string;
-          user_id?: string;
-          vector?: string | null;
-        };
+          created_at?: string
+          id?: string
+          instruction?: string | null
+          title?: string
+          updated_at?: string
+          user_id?: string
+          vector?: string | null
+        }
         Relationships: [
           {
-            foreignKeyName: "notes_user_id_fkey";
-            columns: ["user_id"];
-            isOneToOne: false;
-            referencedRelation: "users";
-            referencedColumns: ["id"];
+            foreignKeyName: "notes_user_id_fkey"
+            columns: ["user_id"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
           },
-        ];
-      };
-    };
+        ]
+      }
+    }
     Views: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Functions: {
       binary_quantize:
         | {
             Args: {
-              "": string;
-            };
-            Returns: unknown;
+              "": string
+            }
+            Returns: unknown
           }
         | {
             Args: {
-              "": unknown;
-            };
-            Returns: unknown;
-          };
+              "": unknown
+            }
+            Returns: unknown
+          }
       halfvec_avg: {
         Args: {
-          "": number[];
-        };
-        Returns: unknown;
-      };
+          "": number[]
+        }
+        Returns: unknown
+      }
       halfvec_out: {
         Args: {
-          "": unknown;
-        };
-        Returns: unknown;
-      };
+          "": unknown
+        }
+        Returns: unknown
+      }
       halfvec_send: {
         Args: {
-          "": unknown;
-        };
-        Returns: string;
-      };
+          "": unknown
+        }
+        Returns: string
+      }
       halfvec_typmod_in: {
         Args: {
-          "": unknown[];
-        };
-        Returns: number;
-      };
+          "": unknown[]
+        }
+        Returns: number
+      }
       hnsw_bit_support: {
         Args: {
-          "": unknown;
-        };
-        Returns: unknown;
-      };
+          "": unknown
+        }
+        Returns: unknown
+      }
       hnsw_halfvec_support: {
         Args: {
-          "": unknown;
-        };
-        Returns: unknown;
-      };
+          "": unknown
+        }
+        Returns: unknown
+      }
       hnsw_sparsevec_support: {
         Args: {
-          "": unknown;
-        };
-        Returns: unknown;
-      };
+          "": unknown
+        }
+        Returns: unknown
+      }
       hnswhandler: {
         Args: {
-          "": unknown;
-        };
-        Returns: unknown;
-      };
+          "": unknown
+        }
+        Returns: unknown
+      }
       insert_note_content: {
         Args: {
-          p_note_id: string;
-          p_content: string;
-          p_source_url: string;
-          p_content_type: string;
-        };
-        Returns: undefined;
-      };
+          p_note_id: string
+          p_content: string
+          p_source_url: string
+          p_content_type: string
+        }
+        Returns: undefined
+      }
       insert_note_results: {
         Args: {
-          p_note_id: string;
-          contents: Json;
-        };
-        Returns: undefined;
-      };
+          p_note_id: string
+          contents: Json
+        }
+        Returns: undefined
+      }
       ivfflat_bit_support: {
         Args: {
-          "": unknown;
-        };
-        Returns: unknown;
-      };
+          "": unknown
+        }
+        Returns: unknown
+      }
       ivfflat_halfvec_support: {
         Args: {
-          "": unknown;
-        };
-        Returns: unknown;
-      };
+          "": unknown
+        }
+        Returns: unknown
+      }
       ivfflathandler: {
         Args: {
-          "": unknown;
-        };
-        Returns: unknown;
-      };
+          "": unknown
+        }
+        Returns: unknown
+      }
       l2_norm:
         | {
             Args: {
-              "": unknown;
-            };
-            Returns: number;
+              "": unknown
+            }
+            Returns: number
           }
         | {
             Args: {
-              "": unknown;
-            };
-            Returns: number;
-          };
+              "": unknown
+            }
+            Returns: number
+          }
       l2_normalize:
         | {
             Args: {
-              "": string;
-            };
-            Returns: string;
+              "": string
+            }
+            Returns: string
           }
         | {
             Args: {
-              "": unknown;
-            };
-            Returns: unknown;
+              "": unknown
+            }
+            Returns: unknown
           }
         | {
             Args: {
-              "": unknown;
-            };
-            Returns: unknown;
-          };
+              "": unknown
+            }
+            Returns: unknown
+          }
       sparsevec_out: {
         Args: {
-          "": unknown;
-        };
-        Returns: unknown;
-      };
+          "": unknown
+        }
+        Returns: unknown
+      }
       sparsevec_send: {
         Args: {
-          "": unknown;
-        };
-        Returns: string;
-      };
+          "": unknown
+        }
+        Returns: string
+      }
       sparsevec_typmod_in: {
         Args: {
-          "": unknown[];
-        };
-        Returns: number;
-      };
+          "": unknown[]
+        }
+        Returns: number
+      }
       vector_avg: {
         Args: {
-          "": number[];
-        };
-        Returns: string;
-      };
+          "": number[]
+        }
+        Returns: string
+      }
       vector_dims:
         | {
             Args: {
-              "": string;
-            };
-            Returns: number;
+              "": string
+            }
+            Returns: number
           }
         | {
             Args: {
-              "": unknown;
-            };
-            Returns: number;
-          };
+              "": unknown
+            }
+            Returns: number
+          }
       vector_norm: {
         Args: {
-          "": string;
-        };
-        Returns: number;
-      };
+          "": string
+        }
+        Returns: number
+      }
       vector_out: {
         Args: {
-          "": string;
-        };
-        Returns: unknown;
-      };
+          "": string
+        }
+        Returns: unknown
+      }
       vector_send: {
         Args: {
-          "": string;
-        };
-        Returns: string;
-      };
+          "": string
+        }
+        Returns: string
+      }
       vector_typmod_in: {
         Args: {
-          "": unknown[];
-        };
-        Returns: number;
-      };
-    };
+          "": unknown[]
+        }
+        Returns: number
+      }
+    }
     Enums: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     CompositeTypes: {
-      [_ in never]: never;
-    };
-  };
-};
+      [_ in never]: never
+    }
+  }
+}
 
-type PublicSchema = Database[Extract<keyof Database, "public">];
+type PublicSchema = Database[Extract<keyof Database, "public">]
 
 export type Tables<
   PublicTableNameOrOptions extends
@@ -418,7 +458,7 @@ export type Tables<
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
       Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R;
+      Row: infer R
     }
     ? R
     : never
@@ -426,11 +466,11 @@ export type Tables<
         PublicSchema["Views"])
     ? (PublicSchema["Tables"] &
         PublicSchema["Views"])[PublicTableNameOrOptions] extends {
-        Row: infer R;
+        Row: infer R
       }
       ? R
       : never
-    : never;
+    : never
 
 export type TablesInsert<
   PublicTableNameOrOptions extends
@@ -441,17 +481,17 @@ export type TablesInsert<
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I;
+      Insert: infer I
     }
     ? I
     : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
     ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Insert: infer I;
+        Insert: infer I
       }
       ? I
       : never
-    : never;
+    : never
 
 export type TablesUpdate<
   PublicTableNameOrOptions extends
@@ -462,17 +502,17 @@ export type TablesUpdate<
     : never = never,
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U;
+      Update: infer U
     }
     ? U
     : never
   : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
     ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Update: infer U;
+        Update: infer U
       }
       ? U
       : never
-    : never;
+    : never
 
 export type Enums<
   PublicEnumNameOrOptions extends
@@ -485,4 +525,4 @@ export type Enums<
   ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
     ? PublicSchema["Enums"][PublicEnumNameOrOptions]
-    : never;
+    : never
